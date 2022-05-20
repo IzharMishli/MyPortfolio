75663ac6cf1edc9a8a3c3a6bbac4b9055ffd9606
IzharMishli <62725719+IzharMishli@users.noreply.github.com>
2022-05-20 22:47:24 +0300

    route tests

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Portfolio />}/>
         <Route path="/MyPortfolio" element={<Portfolio />}/>
-        <Route path="/credits" element={<Credits />}/>
+        <Route path="/MyPortfolio/credits" element={<Credits />}/>
       </Routes>
     </div>
   );
