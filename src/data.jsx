f7626246001026401fedf9dd06d7c8f09cb2e764
IzharMishli <62725719+IzharMishli@users.noreply.github.com>
2022-05-25 20:48:12 +0300

    jumper gif

--- src/data.jsx
+++ src/data.jsx
@@ -1,21 +1,23 @@
 
 const data = [
     {
-        name: "Ronchu-Web",
-        caption: "Make your best special event with tasty food in affordable prices!, in-site invite is available!",
-        image: "pc-ronchu.png",
-        type: "web",
-        gitlink: "https://github.com/IzharMishli/Ronchu-web",
-        links: [{
-            type: "http",
-            link: "https://ronchu-website.herokuapp.com/"
-        }],
-        tags: ["react","java-script", "mongodb"]
+        name: "Brain Challange",
+        caption: "Test your quickness and improve your brain skills, Try to get the best score",
+        image: "brainChallange.png",
+        type: "gaming",
+        gitlink: "https://github.com/IzharMishli/CBTGAME",
+        links: [
+            {
+                type: "googleplay",
+                link: "https://play.google.com/store/apps/details?id=com.fungames.cbtgame"
+            }
+        ],
+        tags: ["xamarin","firebase", "csharp"]
     },
     {
-        name: "Jumper",
+        name: "Jumper (Alpha Ver)",
         caption: "Addictive multiplayer game that you can play with your friends or random opponents, Try it out!",
-        image: "jumper.png",
+        image: "jumper.gif",
         type: "gaming",
         gitlink: "private",
         links: [
@@ -31,19 +33,18 @@ const data = [
         tags: ["unity", "csharp", "azure"]
     },
     {
-        name: "Brain Challange",
-        caption: "Test your quickness and improve your brain skills, Try to get the best score",
-        image: "brainChallange.png",
-        type: "gaming",
-        gitlink: "https://github.com/IzharMishli/CBTGAME",
-        links: [
-            {
-                type: "googleplay",
-                link: "https://play.google.com/store/apps/details?id=com.fungames.cbtgame"
-            }
-        ],
-        tags: ["xamarin","firebase", "csharp"]
+        name: "Ronchu-Web",
+        caption: "Make your best special event with tasty food in affordable prices!, in-site invite is available!",
+        image: "pc-ronchu.png",
+        type: "web",
+        gitlink: "https://github.com/IzharMishli/Ronchu-web",
+        links: [{
+            type: "http",
+            link: "https://ronchu-website.herokuapp.com/"
+        }],
+        tags: ["react","java-script", "mongodb"]
     },
+    
     {
         name: "Angle detector",
         caption: "Algorithm that calculates plane's rolling angle based on computer vision!",
@@ -51,6 +52,18 @@ const data = [
         gitlink: "https://github.com/IzharMishli/OpenCV-PlaneAngleDetector",
         type: "computer-vision",
         tags: ["opencv", "python"]
+    },
+    {
+        name: "Instagram feed",
+        caption: "npm module created by me, can be used to integrated a feed in your react web app. visit my npm page",
+        image: "my-npm.png",
+        gitlink: "https://github.com/IzharMishli/insta-react-feed",
+        links: [{
+            type: "http",
+            link: "https://www.npmjs.com/package/insta-react-feed"
+        }],
+        type: "other",
+        tags: ["react","java-script"]
     },
     {
         name: "My Portfolio",
@@ -65,18 +78,7 @@ const data = [
         tags: ["react","java-script"]
     }
     ,
-    {
-        name: "Instagram feed",
-        caption: "npm module created by me, can be used to integrated a feed in your react web app. visit my npm page",
-        image: "my-npm.png",
-        gitlink: "https://github.com/IzharMishli/insta-react-feed",
-        links: [{
-            type: "http",
-            link: "https://www.npmjs.com/package/insta-react-feed"
-        }],
-        type: "other",
-        tags: ["react","java-script"]
-    },
+    
     {
         name: "Flight Record",
         caption: "use csv file with flight data to view visually the flight and other telemetry like position and errors",
